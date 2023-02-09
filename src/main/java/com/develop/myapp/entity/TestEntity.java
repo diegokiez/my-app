9a9117bcfdd4835c51db519c57b02a4d54a91cce
diegokiez <diegokiez@gmail.com>
2023-02-08 19:37:38 -0600

    my-app: Version final

---
+++ src/main/java/com/develop/myapp/entity/TestEntity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.develop.myapp.entity;
+
+/**
+ * @author diegokiez
+ *
+ */
+public class TestEntity {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
